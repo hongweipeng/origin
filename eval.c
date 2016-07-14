5fffd9ca9ea4961dba1fb514403c3b73fc862567
hongweipeng <hongweichen8888@sian.com>
2016-07-14 16:19:07 +0800

    add method

--- eval.c
+++ eval.c
@@ -180,16 +180,149 @@ static void eval_binary_int(ORG_Interpreter *inter, ExpressionType op,
         case EQ_EXPRESSION:
             result->u.int_value = left == right;
             break;
-
-
+        case NE_EXPRESSION:
+            result->u.boolean_value = left != right;
+            break;
+        case GT_EXPRESSION:
+            result->u.boolean_value = left > right;
+            break;
+        case GE_EXPRESSION:
+            result->u.boolean_value = left >= right;
+            break;
+        case LT_EXPRESSION:
+            result->u.boolean_value = left < right;
+            break;
+        case LE_EXPRESSION:
+            result->u.boolean_value = left <= right;
+            break;
+        case MINUS_EXPRESSION:              /* FALLTHRU */
+        case FUNCTION_CALL_EXPRESSION:      /* FALLTHRU */
+        case NULL_EXPRESSION:               /* FALLTHRU */
+        case EXPRESSION_TYPE_COUNT_PLUS_1:  /* FALLTHRU */
+        default:
+            printf("");
+            //DBG_panic(("bad case...%d", operator));
     }
 }
 
+static void eval_binary_double(ORG_Interpreter *inter, ExpressionType *op, double left, double right,
+                               ORG_Value *result, int line_number) {
 
+    if (dkc_is_math_operator(op)) {
+        result->type = ORG_BOOLEAN_VALUE;
+    } else if (dkc_is_compare_operator(op)) {
+        result->type = ORG_BOOLEAN_VALUE;
+    } else {
+        //debug
+    }
 
+    switch (op) {
+        case BOOLEAN_EXPRESSION:    /* fail */
+        case INT_EXPRESSION:        /* fail */
+        case DOUBLE_EXPRESSION:     /* fail */
+        case STRING_EXPRESSION:     /* fail */
+        case IDENTIFIER_EXPRESSION: /* fail */
+        case ASSIGN_EXPRESSION:
+            //DBG_panic(("bad case...%d", operator));
+            break;
+        case ADD_EXPRESSION:
+            result->u.double_value = left + right;
+            break;
+        case SUB_EXPRESSION:
+            result->u.double_value = left - right;
+            break;
+        case MUL_EXPRESSION:
+            result->u.double_value = left * right;
+            break;
+        case DIV_EXPRESSION:
+            result->u.double_value = left / right;
+            break;
+        case MOD_EXPRESSION:
+            result->u.double_value = fmod(left, right);
+            break;
+        case LOGICAL_AND_EXPRESSION:        /* fail */
+        case LOGICAL_OR_EXPRESSION:
+            //DBG_panic(("bad case...%d", operator));
+            break;
+        case EQ_EXPRESSION:
+            result->u.int_value = left == right;
+            break;
+        case NE_EXPRESSION:
+            result->u.int_value = left != right;
+            break;
+        case GT_EXPRESSION:
+            result->u.int_value = left > right;
+            break;
+        case GE_EXPRESSION:
+            result->u.int_value = left >= right;
+            break;
+        case LT_EXPRESSION:
+            result->u.int_value = left < right;
+            break;
+        case LE_EXPRESSION:
+            result->u.int_value = left <= right;
+            break;
+        case MINUS_EXPRESSION:              /* fail */
+        case FUNCTION_CALL_EXPRESSION:      /* fail */
+        case NULL_EXPRESSION:               /* fail */
+        case EXPRESSION_TYPE_COUNT_PLUS_1:  /* fail */
+        default:
+            printf("");
+            //DBG_panic(("bad default...%d", operator));
+    }
 
+}
 
+static ORG_Boolean eval_compare_string(ExpressionType op, ORG_Value *left, ORG_Value *right, int line_number) {
+    ORG_Boolean result;
+    int cmp;
+    cmp = strcmp(left->u.string_value, right->u.string_value);
 
+    if (operator == EQ_EXPRESSION) {
+        result = (cmp == 0);
+    } else if (operator == NE_EXPRESSION) {
+        result = (cmp != 0);
+    } else if (operator == GT_EXPRESSION) {
+        result = (cmp > 0);
+    } else if (operator == GE_EXPRESSION) {
+        result = (cmp >= 0);
+    } else if (operator == LT_EXPRESSION) {
+        result = (cmp < 0);
+    } else if (operator == LE_EXPRESSION) {
+        result = (cmp <= 0);
+    } else {
+        char *op_str = crb_get_operator_string(operator);
+        //runtime error
+    }
+    crb_release_string(left->u.string_value);
+    crb_release_string(right->u.string_value);
+
+    return result;
+}
+
+static ORG_Boolean eval_binary_null(ORG_Interpreter *inter, ExpressionType op, ORG_Value *left, ORG_Value *right, int line_number) {
+
+    ORG_Boolean result;
+    if (op == EQ_EXPRESSION) {
+        result = (left->type == ORG_NULL_VALUE && right->type == ORG_NULL_VALUE);
+    } else if (operator == NE_EXPRESSION) {
+        result = !(left->type == CRB_NULL_VALUE && right->type == CRB_NULL_VALUE);
+    } else {
+        char *op_str = crb_get_operator_string(operator);
+        //runtime error
+    }
+    release_if_string(left);
+    release_if_string(right);
+
+    return result;
+}
+
+ORG_String *chain_string(ORG_Interpreter *inter, ORG_String *left, ORG_String *right) {
+    //like  "123" + "adc"
+    int len = strlen(left->string) + strlen(right->string);
+    char *str;
+    ORG_String *ret;
+}
 
 
 
