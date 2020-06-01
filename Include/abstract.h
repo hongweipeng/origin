d571164857397563f96c1abfd3187302e0038ea8
hongweipeng <hongweichen8888@sina.com>
2020-06-01 18:28:51 +0800

    基础文件占坑

---
+++ Include/abstract.h
@@ -0,0 +1,8 @@
+//
+// Created by admin on 2020/6/1.
+//
+
+#ifndef ORIGIN_ABSTRACT_H
+#define ORIGIN_ABSTRACT_H
+
+#endif //ORIGIN_ABSTRACT_H
---
+++ Objects/intobject.c
@@ -0,0 +1,4 @@
+//
+// Created by admin on 2020/6/1.
+//
+
